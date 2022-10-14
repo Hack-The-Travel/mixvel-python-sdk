5c1e5e880789f74482cf8f5af2da7e59ca9268b6
Sergei Popinevskii <gurza000@gmail.com>
2022-10-15 01:22:35 +0300

    add utils for working with enpoints

---
+++ src/mixvel/_internal_utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+mixvel._internal_utils
+~~~~~~~~~~~~~~
+Provides utility functions that are consumed internally by mixvel.
+"""
+
+def is_login_endpoint(endpoint):
+    """Determines if the given endpoint is the login endpoint.
+    
+    :param endpoint: endpoint
+    :type endpoint: str
+    :rtype: bool
+    """
+    return e == "/api/Accounts/login"
+
+
+def request_template(endpoint):
+    """Returns request template filename for the given endpoint.
+
+    :param endpoint: endpoint
+    :type endpoint: str
+    :rtype: str or None
+    """
+    return {
+        "/api/Accounts/login": "accounts_login.xml",
+        "/api/Order/airshopping": "order_airshopping.xml",
+    }.get(endpoint, None)
