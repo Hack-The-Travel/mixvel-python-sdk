1a325743c90f32ac5dc8a67bccff66bd61214868
Sergei Popinevskii <gurza000@gmail.com>
2022-10-16 02:18:44 +0300

    define individual model

--- src/mixvel/models.py
+++ src/mixvel/models.py
@@ -17,6 +17,29 @@ class AnonymousPassenger:
         self.ptc = ptc
 
 
+class Individual():
+    def __init__(self, given_name, middle_name, surname,
+                 gender, birthdate):
+        """Individual.
+
+        :param given_name: given name
+        :type given_name: str
+        :param middle_name: middle name
+        :type middle_name: str
+        :param surname: surname
+        :type surname: str
+        :param gender: gender, possible values: "M", "F"
+        :type gender: str
+        :param birthdate: birthdate
+        :type birthdate: datetime.datetime
+        """
+        self.given_name = given_name
+        self.middle_name = middle_name
+        self.surname = surname
+        self.gender = gender
+        self.birthdate = birthdate
+
+
 class Leg:
     def __init__(self, origin, destination, departure,
                  cabin="Economy"):
