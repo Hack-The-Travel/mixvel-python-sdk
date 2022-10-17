0f215110e064f5d2fe5a330ccdd8101ed68407e0
Sergei Popinevskii <gurza000@gmail.com>
2022-10-17 23:09:55 +0300

    implement method to retrieve order

--- src/mixvel/_endpoint.py
+++ src/mixvel/_endpoint.py
@@ -27,6 +27,7 @@ def request_template(endpoint):
         "/api/Accounts/login": "accounts_login.xml",
         "/api/Order/airshopping": "order_airshopping.xml",
         "/api/Order/create": "order_create.xml",
+        "/api/Order/retrieve": "order_retrieve.xml",
         "/api/Order/change": "order_change.xml",
         "/api/Order/cancel": "order_cancel.xml",
     }.get(endpoint, None)
