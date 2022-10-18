3e9770b8fa50fd53673ebf0ae3f2a9bfe5c83536
Sergei Popinevskii <gurza000@gmail.com>
2022-10-18 09:40:35 +0300

    implement test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
-import os
-
-import pytest
-from lxml import etree
-
+from .utils import load_response
 from mixvel.utils import lxml_remove_namespace
 
-
-here = os.path.abspath(os.path.dirname(__file__))
+import pytest
 
 
 class TestUtils:
-    @pytest.mark.parametrize('response_file_path', [
-        os.path.join(here, 'responses/accounts/', 'login_error.xml'),
+    @pytest.mark.parametrize('resp_path', [
+        'responses/accounts/login_error.xml',
     ])
-    def test_lxml_remove_namespace(self, response_file_path):
-        response = etree.parse(response_file_path)
-        assert response.find('.//AuthResponse') is None
-        lxml_remove_namespace(response)
-        assert response.find('.//AuthResponse') is not None
+    def test_lxml_remove_namespace(self, resp_path):
+        resp = load_response(resp_path, clean_appdata=False)
+        assert resp.find('.//AuthResponse') is None
+        lxml_remove_namespace(resp)
+        assert resp.find('.//AuthResponse') is not None
---
+++ tests/utils.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+
+from mixvel.utils import lxml_remove_namespace
+
+from lxml import etree
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_response(resp_path,
+                  clean_appdata=True):
+    """Returns a XML response from the given file."""
+    abs_path = os.path.join(here, resp_path)
+    resp = etree.parse(abs_path)
+
+    if not clean_appdata:
+        return resp
+
+    lxml_remove_namespace(resp)
+    return resp.find('.//Body/AppData/')
