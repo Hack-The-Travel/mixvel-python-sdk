9a0bae1fd9ad0d568dc37301eb3cefcf2d77188f
Sergei Popinevskii <gurza000@gmail.com>
2022-10-19 15:54:39 +0300

    fix copy-paste error in test name

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -4,10 +4,11 @@ from mixvel.parsers import is_cancel_success
 
 import pytest
 
+
 class TestParsers:
     @pytest.mark.parametrize('resp_path', [
         'responses/order/cancel_success.xml',
     ])
-    def test_lxml_remove_namespace(self, resp_path):
+    def test_is_cancel_success(self, resp_path):
         resp = load_response(resp_path)
         assert is_cancel_success(resp)
