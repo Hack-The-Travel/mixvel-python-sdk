3e1cc76db6515b4669cee7abfbde9bab49f6171f
Sergei Popinevskii <gurza000@gmail.com>
2022-11-04 21:31:39 +0300

    fix: add parser of order view response

--- src/mixvel/__version__.py
+++ src/mixvel/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "mixvel"
 __description__ = "Client for MixVel API"
 __url__ = "https://github.com/Hack-The-Travel/mixvel-python-sdk"
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __author__ = "Sergei Popinevskii"
 __author_email__ = "sergei@yggdr.io"
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -9,7 +9,10 @@ from mixvel._parsers import (
     parse_amount, parse_booking, parse_fare_component, parse_fare_detail,
     parse_mix_order, parse_order_item, parse_order, parse_price,
     parse_tax,
-)  # type parsers
+)
+from mixvel.models import (
+    OrderViewResponse,
+)
 from mixvel.models import (
     Amount, Booking, FareComponent, FareDetail,
     MixOrder, Order, OrderItem, Price,
@@ -27,42 +30,14 @@ class TestParsers:
         resp = load_response(resp_path)
         assert is_cancel_success(resp)
 
-    def test_parse_order_view(self):
-        mix_order = MixOrder(
-            "00999-210624-MEE0458",  # mix order id
-            [
-                Order(
-                    "00999-210624-OEE0459",
-                    [
-                        Booking("04G82X"),
-                    ],
-                    datetime.datetime(2021, 9, 23, 0, 40, 0),  # tttl
-                    Amount(653800, "RUB"),  # order total amount
-                ),
-            ],
-            Amount(653800, "RUB")  # total amount
-        )
-        resp = load_response("responses/order/view.xml")
+    @pytest.mark.parametrize("resp_path", [
+        "responses/order/view.xml",
+    ])
+    def test_parse_order_view(self, resp_path):
+        resp = load_response(resp_path)
         got = parse_order_view(resp)
-
-        assert got.mix_order_id == mix_order.mix_order_id
-        assert got.total_amount.amount == mix_order.total_amount.amount
-        assert got.total_amount.cur_code == mix_order.total_amount.cur_code
-
-        # orders
-        assert len(got.orders) == len(mix_order.orders)
-        for i in range(len(got.orders)):
-            assert got.orders[i].order_id == mix_order.orders[i].order_id
-            assert got.orders[i].deposit_timelimit == mix_order.orders[i].deposit_timelimit
-            assert got.orders[i].total_price.amount == mix_order.orders[i].total_price.amount
-            assert got.orders[i].total_price.cur_code == mix_order.orders[i].total_price.cur_code
-
-            # booking_refs
-            assert len(got.orders[i].booking_refs) == len(mix_order.orders[i].booking_refs)
-            for j in range(len(got.orders[i].booking_refs)):
-                assert got.orders[i].booking_refs[j].booking_id \
-                    == mix_order.orders[i].booking_refs[j].booking_id
-
+        assert isinstance(got, OrderViewResponse)
+        assert isinstance(got.mix_order, MixOrder)
 
 class TestTypeParsers:
     @pytest.mark.parametrize("model_path,want", [
